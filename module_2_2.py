99510d1c876b3cad99909f5eef80f3c02c5ceefd
Sergi0iv <rickitir2.0@gmail.com>
2024-08-07 21:34:11 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,10 +1,10 @@
-first = 13
+first = 12
 second = 13
-third = 13
+third = int(input('Число: '))
 
 if second==third and first==third:
     print(3)
-elif second==first:
+elif second==first or second==third:
     print(2)
 else:
     print(0)
